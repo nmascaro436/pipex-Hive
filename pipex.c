27c7b15bec4b8eba09c5c3dbecaedc4a0d87641a
Nuria Mascaro Triay <nmascaro@student.hive.fi>
2025-07-26 11:53:45 +0300

    feat: properly handling exit status

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: nmascaro <nmascaro@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 11:38:23 by nmascaro          #+#    #+#             */
-/*   Updated: 2025/07/24 14:13:48 by nmascaro         ###   ########.fr       */
+/*   Updated: 2025/07/26 11:51:28 by nmascaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,30 @@ static void	close_fds(int infile, int outfile, int pipefd[2])
 	close(pipefd[1]);
 }
 /**
+ * Waits for two child processes to end.
+ * It retrieves their exit statuses, if the second child
+ * terminated normally, its exit status is returned.
+ * Otherwise, the function returns 1.
+ */
+
+ static int	wait_for_children(pid_t pid1, pid_t pid2)
+ {
+	int	status1;
+	int	status2;
+
+	waitpid(pid1, &status1, 0);
+	waitpid(pid2, &status2, 0);
+	if (WIFEXITED(status2))
+		return (WEXITSTATUS(status2));
+	return (1);
+ }
+/**
  * Entry point of the program.
  * Validates arguments, opens input/output files, creates a pipe,
  * forks two child processes to run the commands, and waits for them.
  * Handles setup and cleanup of all fd and system resources.
+ * Reuses 'infile' variable to store exit status, since infile fd
+ * is no longer needed.
  */
 
 int	main(int argc, char *argv[], char *const envp[])
@@ -48,14 +68,13 @@ int	main(int argc, char *argv[], char *const envp[])
 	}
 	infile = open_infile(argv[1]);
 	outfile = open_outfile(argv[4]);
-	if (infile == -1 || outfile == -1)
+	if (outfile == -1)
 		exit(EXIT_FAILURE);
 	if (pipe(pipefd) == -1)
 		system_call_error("pipe");
 	pid1 = first_child(argv[2], infile, pipefd, envp);
 	pid2 = second_child(argv[3], outfile, pipefd, envp);
 	close_fds(infile, outfile, pipefd);
-	waitpid(pid1, NULL, 0);
-	waitpid(pid2, NULL, 0);
-	return (0);
+	infile = wait_for_children(pid1, pid2);
+	return (infile);
 }
