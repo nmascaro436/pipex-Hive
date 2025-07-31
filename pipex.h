48644e84dc2f3f417a5375fcc4e29e27157da53f
Nuria Mascaro Triay <nmascaro@student.hive.fi>
2025-07-31 11:27:42 +0300

    fix: updated function prototypes

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: nmascaro <nmascaro@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 14:35:51 by nmascaro          #+#    #+#             */
-/*   Updated: 2025/07/28 09:25:39 by nmascaro         ###   ########.fr       */
+/*   Updated: 2025/07/31 11:26:46 by nmascaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,21 @@
 # include <sys/wait.h> //For waitpid(), WIFEXITED(), WEXITSTATUS()
 # include <unistd.h> //For fork(), pipe(), dup2(), close(), access()
 
+// File operations
 int		open_infile(const char *path);
 int		open_outfile(const char *path);
+
+// Error handling
 void	print_file_error(const char *filename);
 void	system_call_error(const char *message);
 void	logic_error(const char *message);
 void	print_command_error(const char *command);
-pid_t	first_child(char *cmd1, int infile, int pipefd[2], char *const envp[]);
-pid_t	second_child(char *cmd2, int outfile, int pipefd[2],
-			char *const envp[]);
+
+// Process management 
+pid_t	first_child(char *cmd1, int fds[2], int pipefd[2], char *const envp[]);
+pid_t	second_child(char *cmd2, int fds[2], int pipefd[2], char *const envp[]);
+
+// Command path resolution
 char	*get_command_path(char *cmd, char *const envp[]);
 int		is_cmd_path(char *cmd);
 void	free_paths(char **paths);
